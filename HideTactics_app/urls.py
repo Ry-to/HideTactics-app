deb8fb6372bad1b0fb25eee9f7539ccf529bd6de
Ry-to <watanaberyuto0601@gmail.com>
2025-10-02 23:51:42 +0900

    start

--- HideTactics_app/urls.py
+++ HideTactics_app/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("rkth_game/", include("rkth_game/")),
+    path("rkth_game/", include("rkth_game.urls")),
 ]
