96876267987a47d1fc1f2436463cd012b7b9ef10
Koo-raven-ks <raven_ks@icloud.com>
2025-10-16 14:13:09 +0900

    Co-authored-by: Ry-to <Ry-to@users.noreply.github.com>

--- rkth_game/static/rkth_game/js/top.js
+++ rkth_game/static/rkth_game/js/top.js
@@ -1,1 +1,11 @@
-console.log("hello")
+document.addEventListener('DOMContentLoaded', function () {
+    const startBtn = document.querySelector('.startBtn');
+    const selectMenu = document.querySelector('.select');
+    const title = document.querySelector('h1');
+
+    startBtn.addEventListener('click', function () {
+        startBtn.style.display = 'none';
+        title.style.display = 'none';
+        selectMenu.style.display = 'block';
+    });
+});
